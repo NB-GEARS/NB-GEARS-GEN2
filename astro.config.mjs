1c389e72b44e830b4bb72f0b19f96cda0b7dc192
Siravij Praevisavakij <siravijbb@gmail.com>
2023-08-17 01:57:55 +0700

    favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import { readingTime } from './src/modules/remark/readingTime.mjs'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.nbgears.tech/',
+  site: 'https://siravijbb.me/',
   compressHTML: true,
   integrations: [
     sitemap({
