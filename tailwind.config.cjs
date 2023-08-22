4683f7382980cd6eab53c09051c1777c5f3b46fb
Siravij Praevisavakij <siravijbb@gmail.com>
2023-08-22 22:05:11 +0700

    initial:gen2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}',"./node_modules/flowbite/**/*.js",],
   darkMode: 'media',
   mode: 'jit',
   theme: {
@@ -35,5 +35,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography','')],
 }
